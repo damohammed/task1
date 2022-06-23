ed672b554c95d600702e2cf6bdaf896692aeaaac
damohammed <mo7amad999999@gmail.com>
2022-06-23 21:38:11 +0500

    variables

--- tasks/task3/Extras/variables.tf
+++ tasks/task3/Extras/variables.tf
@@ -1,6 +1,15 @@
-source_image_reference {
+variable "source_image" {
+       description = "image reference"
+       type = list(object({
+        publisher  = string
+        offer      = string
+        sku        = string
+        version    = string
+       }))
+     default = [{        
        publisher = "OpenLogic"
        offer     = "CentOS"
        sku       = "7.5"
        version   = "latest"
+      }]
      }
